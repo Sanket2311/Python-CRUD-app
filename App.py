e542cd524b821e4f76557edf6bd5fd2424549ecc
Sanket Patel <sanket.sankry@gmail.com>
2021-03-28 20:50:22 -0400

    another change

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key = "Secret key"
  
 #SqlAlchemy Database Configuration With Mysql
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://n1kgmt4q3oqm5vd8:yzay4mron21zi4np@u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g6k28qe8qz4sd3us"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("mysql://n1kgmt4q3oqm5vd8:yzay4mron21zi4np@u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g6k28qe8qz4sd3us")
  #"mysql://b1131a4569335f:'44bfbd1d'@us-cdbr-east-03.cleardb.com/crud"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
